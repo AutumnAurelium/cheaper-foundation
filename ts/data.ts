664486a86cd38a5f76ace5fc86ea647a733fca71
AutumnAurelium <88015631+AutumnAurelium@users.noreply.github.com>
2024-11-14 01:17:52 -0500

    first commit

---
+++ ts/data.ts
@@ -0,0 +1,13 @@
+data.raw.recipe["foundation"].ingredients = [
+    {type: "item", name: "refined-concrete", amount: 20},
+    {type: "item", name: "tungsten-plate", amount: 5},
+    {type: "item", name: "lithium-plate", amount: 5}
+];
+
+data.raw.recipe["foundation"].results = [
+    {type: "item", name: "foundation", amount: 5}
+];
+
+data.raw.recipe["foundation"].category = "basic-crafting";
+
+data.raw.item["foundation"].weight = 10_000; // rocket capacity: 100
